11bfd7b8ce98b5ce262eb86ba8bca9ca4e7127d8
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2026-04-25 01:51:39 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,17 @@
-GOOS=windows GOARCH=amd64 go build -o ipatool-main-windows-amd64.exe
-GOOS=windows GOARCH=arm64 go build -o ipatool-main-windows-arm64.exe
+$ErrorActionPreference = "Stop"
+
+$env:GOPROXY = "https://goproxy.cn,direct"
+$env:GOSUMDB = "sum.golang.google.cn"
+
+$env:GOOS = "windows"
+$env:GOARCH = "amd64"
+go build -o ipatool-main-windows-amd64.exe
+
+$env:GOOS = "windows"
+$env:GOARCH = "arm64"
+go build -o ipatool-main-windows-arm64.exe
+
+Remove-Item Env:\GOOS -ErrorAction SilentlyContinue
+Remove-Item Env:\GOARCH -ErrorAction SilentlyContinue
+Remove-Item Env:\GOPROXY -ErrorAction SilentlyContinue
+Remove-Item Env:\GOSUMDB -ErrorAction SilentlyContinue
