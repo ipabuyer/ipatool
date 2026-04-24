422aa4c349ca5113a3852bf01c4369495c1c22d3
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2026-04-25 01:59:18 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -5,11 +5,11 @@ $env:GOSUMDB = "sum.golang.google.cn"
 
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
-go build -o ipatool-main-windows-amd64.exe
+go build -trimpath -ldflags="-s -w" -o ipatool-main-windows-amd64.exe
 
 $env:GOOS = "windows"
 $env:GOARCH = "arm64"
-go build -o ipatool-main-windows-arm64.exe
+go build -trimpath -ldflags="-s -w" -o ipatool-main-windows-arm64.exe
 
 Remove-Item Env:\GOOS -ErrorAction SilentlyContinue
 Remove-Item Env:\GOARCH -ErrorAction SilentlyContinue
