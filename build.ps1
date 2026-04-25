f460e17bdb258c07999783f146e1e7fed8bd9986
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2026-04-25 11:00:08 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,16 +2,20 @@ $ErrorActionPreference = "Stop"
 
 $env:GOPROXY = "https://goproxy.cn,direct"
 $env:GOSUMDB = "sum.golang.google.cn"
-
 $env:GOOS = "windows"
-$env:GOARCH = "amd64"
-go build -trimpath -ldflags="-s -w" -o ipatool-main-windows-amd64.exe
 
-$env:GOOS = "windows"
-$env:GOARCH = "arm64"
-go build -trimpath -ldflags="-s -w" -o ipatool-main-windows-arm64.exe
+try {
+    $env:GOARCH = "amd64"
+    go build -trimpath -ldflags="-s -w" -o ipatool-main-windows-amd64.exe
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-Remove-Item Env:\GOOS -ErrorAction SilentlyContinue
-Remove-Item Env:\GOARCH -ErrorAction SilentlyContinue
-Remove-Item Env:\GOPROXY -ErrorAction SilentlyContinue
-Remove-Item Env:\GOSUMDB -ErrorAction SilentlyContinue
+    $env:GOARCH = "arm64"
+    go build -trimpath -ldflags="-s -w" -o ipatool-main-windows-arm64.exe
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+}
+finally {
+    Remove-Item Env:\GOOS -ErrorAction SilentlyContinue
+    Remove-Item Env:\GOARCH -ErrorAction SilentlyContinue
+    Remove-Item Env:\GOPROXY -ErrorAction SilentlyContinue
+    Remove-Item Env:\GOSUMDB -ErrorAction SilentlyContinue
+}
